df6455e9cfe4461fad0f6a4566283cf81fb44b46
Jos <josroossien@hotmail.com>
2016-01-14 00:46:45 +0000

    Add some general config options for portal guns.

--- src/main/java/com/jroossien/portalguns/config/PluginCfg.java
+++ src/main/java/com/jroossien/portalguns/config/PluginCfg.java
@@ -1,9 +1,34 @@
 package com.jroossien.portalguns.config;
 
+import com.jroossien.portalguns.util.Parse;
+import org.bukkit.Color;
+
 public class PluginCfg extends EasyConfig {
+
+    private Color primary;
+    private Color secondary;
+
+    public int portalgun__maxUseDistance = 32;
+    public String portalgun__primaryColor = "0,80,255";
+    public String portalgun__secondaryColor = "255,200,0";
 
     public PluginCfg(String fileName) {
         this.setFile(fileName);
         load();
+    }
+
+    @Override
+    public void load() {
+        primary = Parse.Color(portalgun__primaryColor);
+        secondary = Parse.Color(portalgun__secondaryColor);
+        super.load();
+    }
+
+    public Color getPrimaryColor() {
+        return primary;
+    }
+
+    public Color getSecondaryColor() {
+        return secondary;
     }
 }
